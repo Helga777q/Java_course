c6e7eeda77046c09af8e2d1f154edf13a3dfd39d
Helga777q <helga.qa72@gmail.com>
2020-10-08 19:22:59 +0300

    L3 - рефакторинг GroupDеletiontests - добавлен @BeforeMethod

--- adressbook-web-tests/src/test/java/addressbook/tests/tests/GroupDeletionTests.java
+++ adressbook-web-tests/src/test/java/addressbook/tests/tests/GroupDeletionTests.java
@@ -1,17 +1,23 @@
 package addressbook.tests.tests;
 
 import addressbook.tests.model.GroupData;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class GroupDeletionTests extends TestBase {
 
-
-  @Test
-  public void testGroupDeletion() throws Exception {
+  @BeforeMethod
+  public void groupDeletionPreconditions(){
     app.getNavigationHelper().goToGroupPage();
     if (!app.getGroupHelper().isGroupPresent()){
       app.getGroupHelper().createGroup(new GroupData("test1", "test2-header", null) );
     }
+
+  }
+
+
+  @Test
+  public void testGroupDeletion() throws Exception {
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
