124a49b5acc84599623b80e7652f1fdf35bcf510
Helga777q <helga.qa72@gmail.com>
2020-10-11 22:25:14 +0300

    L4 - GroupCreationTests - поиск макс значения с помощью анонимных функций

--- adressbook-web-tests/src/test/java/addressbook/tests/tests/GroupCreationTests.java
+++ adressbook-web-tests/src/test/java/addressbook/tests/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import addressbook.tests.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -21,14 +22,10 @@ public class GroupCreationTests  extends TestBase {
     // Assert.assertEquals(after1,  before1+1);
     Assert.assertEquals(after.size(), before.size()+1);
 
-    int max =0;
-    for (GroupData g: after ){
-      if (g.getId()>max){
-        max = g.getId();
+    //Comparator<? super GroupData> byId = (Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+    //int max= after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
 
-      }
-    }
-    group.setId(max);
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
