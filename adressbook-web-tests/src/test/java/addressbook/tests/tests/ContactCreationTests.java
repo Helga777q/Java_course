8acb764f70f24cb756972d8c9405fa0b8ca8d8fc
Helga777q <helga.qa72@gmail.com>
2020-10-13 20:21:40 +0300

    L4 - Лямбда-выражения  для тестов класса GroupCreationTests, ContactCreationTests

--- adressbook-web-tests/src/test/java/addressbook/tests/tests/ContactCreationTests.java
+++ adressbook-web-tests/src/test/java/addressbook/tests/tests/ContactCreationTests.java
@@ -7,6 +7,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -35,15 +36,7 @@ public class ContactCreationTests extends TestBase {
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size()+1);
 
-    int max=0;
-    for (ContactData c: after){
-      if (c.getId()>max){
-        max=c.getId();
-      }
-    }
-
-
-    contact.setId(max);
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
@@ -65,16 +58,7 @@ public class ContactCreationTests extends TestBase {
     app.getNavigationHelper().goToHomePage();
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size()+1);
-
-
-    int max=0;
-    for (ContactData c: after){
-      if (c.getId()>max){
-        max=c.getId();
-      }
-    }
-
-    contact.setId(max);
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
--- adressbook-web-tests/src/test/java/addressbook/tests/tests/GroupCreationTests.java
+++ adressbook-web-tests/src/test/java/addressbook/tests/tests/GroupCreationTests.java
@@ -4,7 +4,6 @@ import addressbook.tests.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -14,18 +13,12 @@ public class GroupCreationTests  extends TestBase {
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().goToGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    //int before1 = app.getGroupHelper().getGroupCount();
-    GroupData group = new GroupData("test1", null, null);
+    GroupData group = new GroupData("test15", null, null);
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
-    //int after1 = app.getGroupHelper().getGroupCount();
-    // Assert.assertEquals(after1,  before1+1);
     Assert.assertEquals(after.size(), before.size()+1);
-
-    //Comparator<? super GroupData> byId = (Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
-    //int max= after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId();
-
-    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+    int max = after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId();
+    group.setId(max);
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
