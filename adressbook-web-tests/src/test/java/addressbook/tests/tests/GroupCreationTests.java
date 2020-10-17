f1cc9beac7df8bf5fefc1dd18c26efa0b77ac643
Helga777q <helga.qa72@gmail.com>
2020-10-18 00:05:17 +0300

    L5 - GroupVreationTests -проверки: замена сравнений списков на сравнения множеств

--- adressbook-web-tests/src/test/java/addressbook/tests/tests/GroupCreationTests.java
+++ adressbook-web-tests/src/test/java/addressbook/tests/tests/GroupCreationTests.java
@@ -1,29 +1,24 @@
 package addressbook.tests.tests;
 
 import addressbook.tests.model.GroupData;
-import org.testng.Assert;
 import org.testng.annotations.Test;
-import java.util.Comparator;
-import java.util.List;
+import java.util.Set;
+
+import static org.testng.Assert.assertEquals;
 
 public class GroupCreationTests  extends TestBase {
 
   @Test
   public void testGroupCreation() throws Exception {
     app.goTo().groupPage();
-    List<GroupData> before = app.group().list();
+    Set<GroupData> before = app.group().all();
     GroupData group = new GroupData().withName("Test");
     app.group().create(group);
-    List<GroupData> after = app.group().list();
-    Assert.assertEquals(after.size(), before.size()+1);
-
-    //get the id of new created group
-    group.withId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
+    Set<GroupData> after = app.group().all();
+    assertEquals(after.size(), before.size()+1);
+    group.withId(after.stream().mapToInt((g)-> g.getId()).max().getAsInt());     //get the id of new created group -max id of the group
     before.add(group);
-    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
-    before.sort(byId);
-    after.sort(byId);
-    Assert.assertEquals(before, after);
+    assertEquals(before, after);
 
   }
 
