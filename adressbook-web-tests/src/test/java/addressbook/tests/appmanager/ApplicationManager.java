ca17d48ee42290a6004e2b8971aaf0b12be2c9a3
Helga777q <helga.qa72@gmail.com>
2020-11-21 20:52:44 +0200

    L10 - запуск удаленного браузера

--- adressbook-web-tests/src/test/java/addressbook/tests/appmanager/ApplicationManager.java
+++ adressbook-web-tests/src/test/java/addressbook/tests/appmanager/ApplicationManager.java
@@ -1,5 +1,6 @@
 package addressbook.tests.appmanager;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -7,11 +8,14 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -34,17 +38,26 @@ public class ApplicationManager {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
     dbHelper = new DbHelper();
-    if (browser.equals(BrowserType.FIREFOX)){
-      wd = new FirefoxDriver();
-    } else if (browser.equals(BrowserType.CHROME)){
-      wd = new ChromeDriver();
-    } else if (browser.equals(BrowserType.IE)){
-      wd = new InternetExplorerDriver();
-    } else if (browser.equals(BrowserType.OPERA)){
-      wd = new OperaDriver();
-    } else if (browser.equals(BrowserType.EDGE)){
-      wd = new EdgeDriver();
+
+    if ("".equals(properties.getProperty("selenium.server"))){
+      if (browser.equals(BrowserType.FIREFOX)){
+        wd = new FirefoxDriver();
+      } else if (browser.equals(BrowserType.CHROME)){
+        wd = new ChromeDriver();
+      } else if (browser.equals(BrowserType.IE)){
+        wd = new InternetExplorerDriver();
+      } else if (browser.equals(BrowserType.OPERA)){
+        wd = new OperaDriver();
+      } else if (browser.equals(BrowserType.EDGE)){
+        wd = new EdgeDriver();
+      }
+    } else{
+      DesiredCapabilities capabilities = new DesiredCapabilities();
+      capabilities.setBrowserName(browser);
+      wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
+
+
 
     wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
     wd.get(properties.getProperty("web.baseUrl"));
